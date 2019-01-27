a8ee23434fb2afbbb34358a0ec0300c6cbc8c288
Piotr Kiliński <piotr.kilinski@gmail.com>
2019-01-27 23:20:20 +0100

    86

--- DatingApp-Spa/src/app/app.module.ts
+++ DatingApp-Spa/src/app/app.module.ts
@@ -2,22 +2,50 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { JwtModule } from '@auth0/angular-jwt';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { NavComponent } from './nav/nav.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './services/error-interceptor';
-import { MemberCardComponent } from './members/member-card/member-card.component';
+
+export function tokenGetter(){
+  return localStorage.getItem('token');
+}
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent, MemberListComponent, ListsComponent, MessagesComponent, MemberCardComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, BsDropdownModule.forRoot()],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
+    MemberListComponent,
+    ListsComponent,
+    MessagesComponent,
+    MemberCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BsDropdownModule.forRoot(),
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        whitelistedDomains: [environment.host],
+        blacklistedRoutes: [environment.host + '/api/auth'],
+      }
+    })
+  ],
   providers: [ErrorInterceptorProvider],
   bootstrap: [AppComponent]
 })
--- DatingApp-Spa/src/app/services/user.service.ts
+++ DatingApp-Spa/src/app/services/user.service.ts
@@ -4,12 +4,6 @@ import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 import { UserDetail } from '../models/user-detail.model';
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    Authorization: 'bearer ' + localStorage.getItem('token')
-  })
-};
-
 @Injectable({
   providedIn: 'root'
 })
@@ -19,10 +13,10 @@ export class UserService {
   constructor(private http: HttpClient) {}
 
   getUsers(): Observable<UserDetail[]> {
-    return this.http.get<UserDetail[]>(this.baseUrl, httpOptions);
+    return this.http.get<UserDetail[]>(this.baseUrl);
   }
 
   getUser(id: number): Observable<UserDetail> {
-    return this.http.get<UserDetail>(`this.baseUrl/${id}`, httpOptions);
+    return this.http.get<UserDetail>(`this.baseUrl/${id}`);
   }
 }
