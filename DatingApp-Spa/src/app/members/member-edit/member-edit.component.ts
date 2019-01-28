f9e978663ea2af86e6dc66d03c8e8ca70c4e46ff
Piotr Kiliński <piotr.kilinski@gmail.com>
2019-01-28 01:26:30 +0100

    96

--- DatingApp-Spa/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-Spa/src/app/members/member-edit/member-edit.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserDetail } from 'src/app/models/user-detail.model';
+import { NgForm } from '@angular/forms';
+import { AlertifyService } from 'src/app/services/alertify.service';
 
 @Component({
   selector: 'app-member-edit',
@@ -8,11 +10,19 @@ import { UserDetail } from 'src/app/models/user-detail.model';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm: NgForm;
   user: UserDetail;
-  constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private alertifyService: AlertifyService
+  ) {}
 
   ngOnInit() {
-    this.activatedRoute.data.subscribe(data => this.user = data['user']);
+    this.activatedRoute.data.subscribe(data => (this.user = data['user']));
   }
 
+  update(): void {
+    this.alertifyService.success('User successfuly edited!');
+    this.editForm.reset(this.user);
+  }
 }
