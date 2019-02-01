e9a4e26fdf42935c152d8ac529a3cfdc0b15af5b
Piotr Kiliński <piotr.kilinski@gmail.com>
2019-02-01 11:49:55 +0100

    167

--- DatingApp-Spa/src/app/members/member-detail/member-detail.component.ts
+++ DatingApp-Spa/src/app/members/member-detail/member-detail.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserDetail } from 'src/app/models/user-detail.model';
-import { NgxGalleryOptions, NgxGalleryAnimation, NgxGalleryImage } from 'ngx-gallery';
+import {
+  NgxGalleryOptions,
+  NgxGalleryAnimation,
+  NgxGalleryImage
+} from 'ngx-gallery';
+import { TabsetComponent } from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-member-detail',
@@ -12,12 +17,18 @@ export class MemberDetailComponent implements OnInit {
   user: UserDetail;
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
+  @ViewChild('memberTabs') memeberTabs: TabsetComponent;
 
   constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
     this.activatedRoute.data.subscribe(data => {
       this.user = data['user'];
+    });
+
+    this.activatedRoute.queryParams.subscribe(queryParams => {
+      const tabId = queryParams['tabId'];
+      this.selectTab(tabId);
     });
 
     this.galleryOptions = [
@@ -37,16 +48,18 @@ export class MemberDetailComponent implements OnInit {
   private getImages(): NgxGalleryImage[] {
     const imageUrls: NgxGalleryImage[] = [];
 
-    for(let i = 0; i < this.user.photos.length; i++) {
-      imageUrls.push(
-        {
-          small: this.user.photos[i].url,
-          medium: this.user.photos[i].url,
-          big: this.user.photos[i].url,
-          description: this.user.photos[i].description
-        }
-      );
+    for (let i = 0; i < this.user.photos.length; i++) {
+      imageUrls.push({
+        small: this.user.photos[i].url,
+        medium: this.user.photos[i].url,
+        big: this.user.photos[i].url,
+        description: this.user.photos[i].description
+      });
     }
     return imageUrls;
+  }
+
+  selectTab(tabId: number) {
+    this.memeberTabs.tabs[tabId].active = true;
   }
 }
