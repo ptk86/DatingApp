f70f832649b4594934c78838f78920654c150a0a
Piotr Kiliński <piotr.kilinski@gmail.com>
2019-02-01 15:52:03 +0100

    fix script

--- run.ps1
+++ run.ps1
@@ -1,9 +1,8 @@
 ﻿$apiPath = ".\DatingApp.Api"
-Start-Process dotnet -ArgumentList "ef database update" -WorkingDirectory $apiPath -Wait
-Start-Process dotnet -ArgumentList "run" -WorkingDirectory $apiPath
-
 $spaPath = ".\DatingApp-Spa"
 Start-Process npm -ArgumentList "install" -WorkingDirectory $spaPath -Wait
+Start-Process dotnet -ArgumentList "ef database update" -WorkingDirectory $apiPath -Wait
+Start-Process dotnet -ArgumentList "run" -WorkingDirectory $apiPath
 Start-Process npm -ArgumentList "start" -WorkingDirectory $spaPath
 
 exit
